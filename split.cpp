510aab32936a7fd6b5a7f15dc2148a500f32cfca
Ying-Yu Ho <heinsolid@gmail.com>
2014-04-30 16:19:37 -0700

    data splitter

--- split.cpp
+++ split.cpp
@@ -1,35 +1,37 @@
 #include <iostream>
 #include <fstream>
+#include <cstdio>
 using namespace std;
 
 int main() {
-    ifstream dtaFile("all.dta");
-    ifstream idxFile("all.idx");
-    ofstream file1("1.dta");
-    ofstream file2("2.dta");
-    ofstream file3("3.dta");
-    ofstream file4("4.dta");
-    ofstream file5("5.dta");
-    char allLine[32];
 
-    int index = 0;
+    char *allLine = NULL, *idxLine = NULL;
+    size_t allLen = 0, idxLen = 0;
+    char buff[32];
 
-    while (idxFile >> index) {
-        dtaFile.getline(allLine, 32);
-        switch (index) {
-            case 1 : file1 << allLine; break;
-            case 2 : file2 << allLine; break;
-            case 3 : file3 << allLine; break;
-            case 4 : file4 << allLine; break;
-            case 5 : file5 << allLine; break;
-            default : break;
-        }
+    FILE *allFile, *idxFile;
+    FILE *outFile[5];
+
+    for (int i = 0; i < 5; i++) {
+        sprintf(buff, "%d.dta", i+1);
+        outFile[i] = fopen(buff, "w");
     }
-    dtaFile.close();
-    idxFile.close();
-    file1.close();
-    file2.close();
-    file3.close();
-    file4.close();
-    file5.close();
+
+    allFile = fopen("all.dta", "r");
+    idxFile = fopen("all.idx", "r");
+
+    while ((getline(&allLine, &allLen, allFile) != -1) && 
+           (getline(&idxLine, &idxLen, idxFile) != -1)) {
+        int index = idxLine[0] - '1';
+        if ((0 <= index) && (index <= 4))
+            fputs(allLine, outFile[index]);
+    }
+
+    free(allLine);
+    free(idxLine);
+
+    fclose(allFile);
+    fclose(idxFile);
+    for (int i = 0; i < 5; i++)
+        fclose(outFile[i]);
 }
